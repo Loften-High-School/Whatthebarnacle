39a88e38881edd2f6c206c972c63e343f2c2e330
QAlto-Sax <meatbrick2005@gmail.com>
2026-05-29 09:04:57 -0400

    mats

---
+++ Assets/scripts/MouseLock.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class MouseLock : MonoBehaviour
+{
+   /* bool locked;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        locked = true;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (locked)
+        {
+            if (Input.GetKeyDown (KeyCode.Escape))
+            {
+                locked = false;
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+            }
+        }
+
+        else{
+            if (Input.GetMouseButtonDown (0))
+            {
+                   locked = false;
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+        }
+    } */
+}
--- Assets/scripts/MultiHitReset.cs
+++ Assets/scripts/MultiHitReset.cs
@@ -7,7 +7,7 @@ public class MultiHitReset : MonoBehaviour
     private int currentHits = 0; // Tracks the current number of hits
 
     // This function is called when another collider enters a trigger collider attached to this object.
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerEnter(Collider other)
     {
         // Check if the colliding object has the "Enemy" tag
         if (other.CompareTag("Enemy"))
@@ -35,16 +35,6 @@ public class MultiHitReset : MonoBehaviour
         // Examples:
         // 1. Reload the current scene:
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-
-        // 2. Reset position to a starting point:
-        // transform.position = new Vector3(0, 0, 0); // Example reset position
-
-        // 3. Reset other player-specific states (e.g., health, power-ups):
-        // PlayerHealth playerHealth = GetComponent<PlayerHealth>();
-        // if (playerHealth != null)
-        // {
-        //     playerHealth.ResetHealth();
-        // }
 
         currentHits = 0; // Reset the hit counter
     }
